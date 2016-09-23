1d164113720ed3de4c33a745069e2ee2060cb267
DavidOdell <davidodell@FHLBMAC7537.fhlbsf-i.com>
2016-09-22 20:05:58 -0700

    changed the mirror to one that works

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aogail@w007.org'
 license          'Apache 2.0'
 description      'Installs/Configures cygwin'
 long_description 'Installs/Configures cygwin'
-version          '0.10.0'
+version          '0.10.1'
 
 supports 'windows', '> 0.0'
 
