8686c0a8354abe2d92d2e10fbd8d7c0be2f586ae
DavidOdell <davidodell@FHLBMAC7537.fhlbsf-i.com>
2016-09-26 14:22:11 -0700

    rev'd version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aogail@w007.org'
 license          'Apache 2.0'
 description      'Installs/Configures cygwin'
 long_description 'Installs/Configures cygwin'
-version          '0.11.0'
+version          '0.12.0'
 
 supports 'windows', '> 0.0'
 
